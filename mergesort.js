b4c5f70fb8ebf86d3b57125029fa5064b3072541
maurolps <boroengenharia@gmail.com>
2023-10-07 19:47:09 -0300

    Second and third merge level sketch

--- mergesort.js
+++ mergesort.js
@@ -1,16 +1,23 @@
 const arr = [9,3,7,5,6,4,8,2];
 
 function merge(l, mid, h) {
-    const arrayL = arr[l];
-    const arrayMid = arr[mid];
-    const arrayH = arr[h];
-    
-    console.log('merge: l:', l, 'mid:', mid, 'h:', h);
-    if(l === mid) {
-       if (arrayL < arrayH) return;
-      arr[l] = arrayH;
-      arr[h] = arrayL;
+  const arrayL = arr[l];
+  const arrayMid = arr[mid];
+  const arrayH = arr[h];
+   if(l === mid) {
+    if (arrayL < arrayH) return;
+    arr[l] = arrayH;
+    arr[h] = arrayL;
+    return;
+  } 
+  
+  console.log('merge: l:', l+1, 'mid:', mid+1, 'h:', h+1);
+  for (let i = l; i <= mid; i++) {
+    console.log(i+1);
+    for (let j = mid+1; j <= h; j++) {
+      console.log('>',j+1);
     }
+  }
 }
 
 function mergeSort (l,h) {
