5195082de3cc7af16d29137725854974867790c9
maurolps <boroengenharia@gmail.com>
2023-10-07 19:15:42 -0300

    Added first level sort

--- mergesort.js
+++ mergesort.js
@@ -1,27 +1,26 @@
 const arr = [9,3,7,5,6,4,8,2];
-let quitCount = 0;
-let quits = 0;
 
-console.log('init...');
-
-function mergeSort (l,h) {
-    console.log('incomming:','l:',l, 'h:',h);
-  if (l < h) {
-    const mid = parseInt((l+h)/2);
-    quitCount++
-    mergeSort(l,mid);
-    quits++;
-    console.log('return1:','l:',l, 'mid:',mid, 'h:',h);
-    quitCount++
-    mergeSort(mid+1, h);
-    quits++;
-    console.log('return2:','l:',l, 'mid:',mid, 'h:',h);
+function merge(l, mid, h) {
+    const arrayL = arr[l];
+    const arrayMid = arr[mid];
+    const arrayH = arr[h];
     
-  } else {
-    console.log('quit: ', quits, '/',quitCount);
-  }
+    console.log('merge: l:', l, 'mid:', mid, 'h:', h);
+    if(l === mid) {
+       if (arrayL < arrayH) return;
+      arr[l] = arrayH;
+      arr[h] = arrayL;
+    }
 }
 
-mergeSort(1,arr.length);
+function mergeSort (l,h) {
+  if (l < h) {
+    const mid = parseInt((l+h)/2);
+    mergeSort(l,mid);
+    mergeSort(mid+1, h);
+    merge(l,mid,h);
+  } 
+}
 
-console.log("end...");
+mergeSort(0,arr.length-1);
+console.log(arr);
