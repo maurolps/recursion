a62eb713d9c0fd1c73b2946839bf069b23b42c83
maurolps <boroengenharia@gmail.com>
2023-10-09 18:19:58 -0300

    Update mocked function

--- mock.js
+++ mock.js
@@ -1,19 +1,27 @@
-const arr = [3, 9, 5, 7, 4, 6, 2, 8]
+const arr = [3, 9, 5, 7]
 
 function merge (l=0, mid=1, h=3) {
   console.log('merge: l:', l+1, 'mid:', mid+1, 'h:', h+1);
-  for (let i = l; i <= mid; i++) {
-    console.log(i+1);
-    for (let j = mid+1; j <= h; j++) {
-      console.log('>',j+1);
+  let j = mid+1;
+  for (let i = l; i <= h; i++) {
+    console.log('i: ', i, 'j:', j);
+    if (j > h) {
+      for (let k = i; k <= mid; k++) {
+        console.log ('rest: ', k)
+      }
+      break;
     }
+
+    if (arr[i] < arr[j]) {
+      console.log(arr[i],' < ',arr[j] ,i, '---', j, '==', i);
+    } else {
+      console.log(arr[i],' > ',arr[j] ,i, '---', j, '==', j);
+      i--;
+      j++;
+    }
+
   }
   console.log(arr);
 }
 
-merge();
-
-// 1---3 == 1
-// 2---3 == 3
-// 2---4 == 4
-// 2...2 == 2
+merge(); // expected output: [3,5,7,9]
